1190f9336942aa986265e4f8199fdd31b7f58e5a
mohamedashraff22 <mohamedashraff2222@gmail.com>
2026-02-22 13:53:13 +0200

    Added llm factory

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -15,11 +15,24 @@ class Settings(
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
-
     FILE_DEFAULT_CHUNCK_SIZE: int
 
     MONGODB_URL: str
     MONGODB_DATABASE: str
+    
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
 
     class Config:
         env_file = ".env"  # specify the name of the environment file to load
